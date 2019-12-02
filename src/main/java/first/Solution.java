c8cf112a3f840c6b98cc1a507aacf23bcfeadaf1
Cristian González <cristiang@empathy.co>
2019-12-02 08:36:16 +0100

    First solution

---
+++ src/main/java/first/Solution.java
@@ -0,0 +1,26 @@
+package first;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Solution {
+
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		Path inputPath = Paths.get(Thread.currentThread().getContextClassLoader().getResource("first/input.txt").toURI());
+		System.out.println(calculateFuelCount(inputPath));
+	}
+
+	private static long calculateFuelCount(Path inputPath) throws IOException {
+		try (Stream<String> moduleMassStream = Files.lines(inputPath)) {
+			return moduleMassStream.map(Long::valueOf)
+								   .map(mass -> mass / 3 - 2)
+								   .reduce(Long::sum)
+								   .orElseThrow(IOException::new);
+		}
+	}
+
+}
