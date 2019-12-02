a5dc5b0156d45a0a40ec244725a6d38522dc3cc1
Cristian González <cristiang@empathy.co>
2019-12-02 09:04:31 +0100

    Part two of first day

--- src/main/java/first/PartOne.java
+++ src/main/java/first/PartOne.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class Solution {
+public class PartOne {
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
 		Path inputPath = Paths.get(Thread.currentThread().getContextClassLoader().getResource("first/input.txt").toURI());
---
+++ src/main/java/first/PartTwo.java
@@ -0,0 +1,28 @@
+package first;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class PartTwo {
+
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		Path inputPath = Paths.get(Thread.currentThread().getContextClassLoader().getResource("second/input.txt").toURI());
+
+		try (Stream<String> moduleMassStream = Files.lines(inputPath)) {
+			moduleMassStream.map(Long::valueOf)
+							.map(mass -> calculateFuel(mass, 0))
+							.reduce(Long::sum)
+							.ifPresent(System.out::println);
+		}
+	}
+
+	private static long calculateFuel(long mass, long acc) {
+		long fuel = mass / 3 - 2;
+		return fuel > 0 ? calculateFuel(fuel, acc + fuel) : acc;
+	}
+
+}
