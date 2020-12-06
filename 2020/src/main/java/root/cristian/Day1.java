f2a91966147f233156d86d06b3ee87ff64120033
Cristian González <cristiang@empathy.co>
2020-12-06 16:59:22 +0100

    add description to each part of day 2

--- 2020/src/main/java/root/cristian/Day1.java
+++ 2020/src/main/java/root/cristian/Day1.java
@@ -1,22 +1,41 @@
 package root.cristian;
 
 /**
- * Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
- * <p/>
- * For example, suppose your expense report contained the following:
- * <ul>
- *      <li>1721</li>
- *      <li>979</li>
- *      <li>366</li>
- *      <li>299</li>
- *      <li>675</li>
- *      <li>1456</li>
- * </ul>
- * In this list, the two entries that sum to 2020 are 1721 and 299.
- * Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+ * --- Day 1: Report Repair ---
  */
 public final class Day1 {
 
+    /**
+     * After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island.
+     * Surely, Christmas will go on without you.
+     * <p/>
+     * The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture
+     * of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but
+     * somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+     * <p/>
+     * To save your vacation, you need to get all fifty stars by December 25th. Collect stars by solving puzzles. Two
+     * puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete
+     * the first. Each puzzle grants one star. Good luck!
+     * <p/>
+     * Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently,
+     * something isn't quite adding up.Specifically, they need you to find the two entries that sum to 2020 and then
+     * multiply those two numbers together.
+     * <p/>
+     * For example, suppose your expense report contained the following:
+     * <ul>
+     *      <li>1721</li>
+     *      <li>979</li>
+     *      <li>366</li>
+     *      <li>299</li>
+     *      <li>675</li>
+     *      <li>1456</li>
+     * </ul>
+     * In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces
+     * 1721 * 299 = 514579, so the correct answer is 514579.
+     *
+     * @param numbers expense report
+     * @return product of two entries which sum is 2020
+     */
     final int first(final int[] numbers) {
         for (final int n : numbers) {
             for (final int m : numbers) {
@@ -29,6 +48,19 @@ public final class Day1 {
         throw new IllegalStateException("Number not found");
     }
 
+    /**
+     * The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left
+     * over from a past vacation. They offer you a second one if you can find three numbers in your expense report that
+     * meet the same criteria.
+     * <p/>
+     * Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them
+     * together produces the answer, 241861950.
+     * <p/>
+     * In your expense report, what is the product of the three entries that sum to 2020?
+     *
+     * @param numbers expense report
+     * @return product of three entries which sum is 2020
+     */
     final int second(final int[] numbers) {
         for (final int n : numbers) {
             for (final int m : numbers) {
--- 2020/src/main/java/root/cristian/Day2.java
+++ 2020/src/main/java/root/cristian/Day2.java
@@ -4,41 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * For example, suppose you have the following list:
- * <ul>
- *      <li>1-3 a: abcde</li>
- *      <li>1-3 b: cdefg</li>
- *      <li>2-9 c: ccccccccc</li>
- * </ul>
- * Each line gives the password policy and then the password. The password policy indicates the lowest and highest
- * number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password
- * must contain a at least 1 time and at most 3 times.
- * <p/>
- * In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but
- * needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of
- * their respective policies.
- * <p/>
- * How many passwords are valid according to their policies?
+ * --- Day 2: Password Philosophy ---
  */
 public final class Day2 {
 
     private interface Policy {
-
         boolean validate(final String password);
-
     }
 
-    private static final class DeprecatedPolicy implements Policy {
-
-        private final int min;
-        private final int max;
-        private final char letter;
-
-        public DeprecatedPolicy(final int min, final int max, final char letter) {
-            this.min = min;
-            this.max = max;
-            this.letter = letter;
-        }
+    private static final record DeprecatedPolicy(int min, int max, char letter) implements Policy {
 
         @Override
         public boolean validate(final String password) {
@@ -48,15 +22,7 @@ public final class Day2 {
 
     }
 
-    private static final class UltraSecurePolicy implements Policy {
-
-        private final int[] positions;
-        private final char letter;
-
-        public UltraSecurePolicy(final char letter, final int... positions) {
-            this.letter = letter;
-            this.positions = positions;
-        }
+    private static final record UltraSecurePolicy(char letter, int... positions) implements Policy {
 
         @Override
         public boolean validate(final String password) {
@@ -65,6 +31,38 @@ public final class Day2 {
 
     }
 
+    /**
+     * Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via
+     * toboggan.
+     * <p/>
+     * The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we
+     * can't log in!" You ask if you can take a look.
+     * <p/>
+     * Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the
+     * Official Toboggan Corporate Policy that was in effect when they were chosen.
+     * <p/>
+     * To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted
+     * database) and the corporate policy when that password was set.
+     * <p/>
+     * For example, suppose you have the following list:
+     * <ul>
+     *      <li>1-3 a: abcde</li>
+     *      <li>1-3 b: cdefg</li>
+     *      <li>2-9 c: ccccccccc</li>
+     * </ul>
+     * Each line gives the password policy and then the password. The password policy indicates the lowest and highest
+     * number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password
+     * must contain a at least 1 time and at most 3 times.
+     * <p/>
+     * In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but
+     * needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of
+     * their respective policies.
+     * <p/>
+     * How many passwords are valid according to their policies?
+     *
+     * @param lines passwords with their policies
+     * @return number of valid passwords
+     */
     final long first(final List<String> lines) {
         int count = 0;
 
@@ -88,6 +86,30 @@ public final class Day2 {
         return count;
     }
 
+    /**
+     * While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan
+     * Corporate Authentication System is expecting.
+     * <p/>
+     * The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job
+     * at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little
+     * differently.
+     * <p/>
+     * Each policy actually describes two positions in the password, where 1 means the first character, 2 means the
+     * second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly
+     * one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the
+     * purposes of policy enforcement.
+     * <p/>
+     * Given the same example list from above:
+     * <ul>
+     *   <li>1-3 a: abcde is valid: position 1 contains a and position 3 does not.</li>
+     *   <li>1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.</li>
+     *   <li>2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.</li>
+     * </ul>
+     * How many passwords are valid according to the new interpretation of the policies?
+     *
+     * @param lines passwords with their policies
+     * @return number of valid passwords
+     */
     final long second(final List<String> lines) {
         int count = 0;
 
