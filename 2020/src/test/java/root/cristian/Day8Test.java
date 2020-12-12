b6cdcb7886d3d84b0a7f2245e16f6eb7e2bd50d7
Cristian González <cristiang@empathy.co>
2020-12-12 17:30:54 +0100

    Day 8 prepared

---
+++ 2020/src/test/java/root/cristian/Day8Test.java
@@ -0,0 +1,54 @@
+package root.cristian;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utils.FilesUtilities;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Day8Test {
+
+    @Test
+    final void exampleFirst() {
+        final String data = """
+                nop +0
+                acc +1
+                jmp +4
+                acc +3
+                jmp -3
+                acc -99
+                acc +1
+                jmp -4
+                acc +6""";
+
+        final Day8 day8 = new Day8();
+        final long result = day8.first(data.lines().collect(Collectors.toList()));
+        Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    final void exampleSecond() {
+
+    }
+
+    @Test
+    final void first() throws IOException {
+        final List<String> data = readInput();
+        final Day8 day8 = new Day8();
+        final long result = day8.first(data);
+        System.out.println("Result is: " + result);
+    }
+
+    @Test
+    final void second() throws IOException {
+
+    }
+
+    private List<String> readInput() throws IOException {
+        return Files.lines(FilesUtilities.getResource("Day8")).collect(Collectors.toList());
+    }
+
+}
